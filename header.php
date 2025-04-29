55a5b3ab4869ca28171b819dc84aac2bbf0cd46a
nicoragg <nicolassouza.sg@gmail.com>
2025-04-28 21:44:38 -0300

    teste novo

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
   <img src="assets\images\tarefa.png" alt="tarefa-logo">
 
   <nav>
-    <a href="?page=home">textonadahaver</a>
+    <a href="?page=home">Dashboard</a>
     <a href="?page=create">Criar Tarefa</a>
     <a href="?page=tasks">Tarefas</a>
     <a href="?page=user">Perfil</a>
