466ce2be54693c42d465ca18fe9dbeb0cb619d7e
phbrugnolo <phbrugnolo@gmail.com>
2025-04-28 21:44:37 -0300

    Identando header

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <header>
-    <img src="assets\images\tarefa.png" alt="tarefa-logo">
+  <img src="assets\images\tarefa.png" alt="tarefa-logo">
 
-    <nav>
-        <a href="?page=home">Dashboard</a>
-        <a href="?page=create">Criar Tarefa</a>
-        <a href="?page=tasks">Tarefas</a>
-        <a href="?page=user">Perfil</a>
+  <nav>
+    <a href="?page=home">Dashboard</a>
+    <a href="?page=create">Criar Tarefa</a>
+    <a href="?page=tasks">Tarefas</a>
+    <a href="?page=user">Perfil</a>
     </div>
-    </nav>
-    
+  </nav>
+
 </header>
